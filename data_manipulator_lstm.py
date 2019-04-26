da11a776a67f3d6c774975620d510cb8040477a0
Jamie <jamiekoerner5@gmail.com>
2019-04-26 08:57:06 -0400

    print

--- data_manipulator_lstm.py
+++ data_manipulator_lstm.py
@@ -45,6 +45,7 @@ def upsample_minority_class(x_train, y_train):
 
 def downsample_majority_class(x_train, y_train):
     #print("downsampling data...")
+    print("here1",y_train.shape)
     # separate majority and minority classes in training data
     df_majority = x_train[np.where(y_train.max(axis=1) == 0)]
     df_minority = x_train[np.where(y_train.max(axis=1) == 1)]
@@ -61,6 +62,6 @@ def downsample_majority_class(x_train, y_train):
     # merge classes again
     df_downsampled_x = np.vstack((df_majority_downsampled, df_minority))
     df_downsampled_y = np.concatenate((np.zeros(NUM_MIN_SAMPLES), np.ones(NUM_MIN_SAMPLES)))
-
+    print("here2",df_downsampled_y.shape)
     # combine majority class with upsampled minority class
     return df_downsampled_x, df_downsampled_y
